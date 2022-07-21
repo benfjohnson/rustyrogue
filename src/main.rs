b4c538f0c589acc86c4023401ffe4dc0d507bf70
Ben Johnson <ben.fisher.johnson@avalara.com>
2022-07-21 06:42:59 -0700

    ch1: hello rust

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use rltk::{GameState, Rltk};
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello Rust World!!");
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Tutorial")
+        .build()?;
+
+    let game_state = State {};
+    rltk::main_loop(context, game_state)
+}
